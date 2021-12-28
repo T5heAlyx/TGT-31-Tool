ab9bfa3209ebdc6d55da17bb4584c3e4090078ec
T5heAlyx <90022171+T5heAlyx@users.noreply.github.com>
2021-12-28 16:40:16 +0300

    Add files via upload

---
+++ TGT 31 Çekme Tool.py
@@ -0,0 +1,100 @@
+#Made By Alyx#4325 31 Çekme Tool
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+while (True):
+
+
+ credits = """
+  _________________________________________
+ [      ###### Made By Alyx ######         ]
+ [     ###### Turkish Guy Tim ######       ]
+ [     Youtube: Turkish Guy Tim            ]
+ [ Discord: https://discord.gg/eZKQWcmuNy  ]
+ [ 31 Kiti, 31 Çekerken Bizi Hatırlayın... ]
+ [_________________________________________]
+ """
+
+ print(credits)
+ print("[%] 31 Çekmek Herşeydir...")
+ print("_________________________________________________________________________")
+ input("[#] 31 Çekme Özel Tooluna Hoşgeldiniz! | Devam Etmek İçin Enter'a Basınız... \n")
+ input("[#] UYARI: Bu Tool (+18) İçerik Barındırmaktadır Ailenizin Yanında Açmayın! | Devam Etmek İçin Enter'a Basınız... \n\n")
+ print("___________________________________________________________")
+ print("[%] NOT: Sitelerin Açılması İçin VPN'e İhtiyacınız Vardır!|")
+ print("__________________________________________________________|")
+ print("[%] NOT: Sağlığınız İçin 31 Çekerken Sabun Kullanınız!    | \n[%] NOT: İyi 31 Çekmeler Dilerim :) -Alyx                 |")
+ print("__________________________________________________________|")
+ print("[%] NOT: Yandex Porn VPN Gerektirmeyen Porn Hizmetidir!   |")
+ print("__________________________________________________________|_______________________")
+ porn = input("[#] Bir Site Seçiniz | PornHub [1] | xnxx [2] | Doeda [3] | Yandex Porn [4] >>> ")
+ print("")
+
+ if porn == "4":
+    secim = input("\n[#] Pornonuz Türk'mü Olsun Yabancımı? | Türk [1] | Yabancı [2] >>>")
+
+ dev = input("[#] Devam Etmek İstediğinizden Eminmisiniz? [Y/n]")
+ if dev == "Y" or dev == "y":
+     pass
+
+ elif dev == "H" or dev == "h":
+     driver.quit()
+
+ else:
+     pass
+
+
+ driver = webdriver.Chrome()
+ time.sleep(2)
+
+ if porn == "1":
+     time.sleep(1)
+     driver.maximize_window()
+     driver.get("www.pornhub.com")
+
+ elif porn == "2":
+     time.sleep(1)
+     driver.maximize_window()
+     driver.get("www.xnxx.com")
+
+ elif porn == "3":
+     time.sleep(1)
+     driver.maximize_window()
+     driver.get("www.doeda.com")
+
+ elif secim == "1":
+     driver.get("https://yandex.com.tr/")
+     time.sleep(1)
+     driver.maximize_window()
+     driver.find_element_by_xpath('// *[ @ id = "text"]').send_keys("türk sikiş izle")
+     driver.find_element_by_xpath('/html/body/div[1]/div[3]/div/div[2]/div/div[2]/form/div[2]/button').click()
+     driver.find_element_by_xpath('/html/body/div[2]/nav/ul/li[3]/div[1]/a').click()
+
+ elif secim == "2":
+     driver.get("https://yandex.com.tr/")
+     time.sleep(1)
+     driver.maximize_window()
+     driver.find_element_by_xpath('// *[ @ id = "text"]').send_keys("porn ")
+     driver.find_element_by_xpath('/html/body/div[1]/div[3]/div/div[2]/div/div[2]/form/div[2]/button').click()
+     driver.find_element_by_xpath('/html/body/div[2]/nav/ul/li[3]/div[1]/a').click()
+
+
+ time.sleep(2)
+ print("_________________________________________________________________________________________________\n")
+ devam = input("[#] İşlemi Sonlandırmak İçin: [1] | Devam Etmek İçin: [2] | Discordumuza Gelmek İçin: [3] Yazınız >>>")
+
+ if devam == "1":
+     break
+     driver.quit()
+
+ elif devam == "3":
+     discord = webdriver.Chrome()
+     discord.maximize_window()
+     discord.get("https://discord.gg/eZKQWcmuNy")
+
+ else:
+     pass
+
+ # Made By Alyx#4325 31 Çekme Tool
